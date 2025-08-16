f08e0736884cb7225e969043cb771cbee6af78dc
Cailee <caileeaverill@gmail.com>
2025-08-15 20:39:31 -0400

    Refactor server structure and modularize notes API
    Moved server entry point to src/server.js and updated package.json scripts accordingly. Split notes API logic into separate controller and routes files for better modularity and maintainability.

---
+++ backend/src/controllers/notesController.js
@@ -0,0 +1,23 @@
+export const getAllNotes = (req, res) => {
+    res.status(201).json({
+        message: "Post received successfully"
+    })
+}
+
+export const createNote = (req, res) => {
+    res.status(201).json({
+        message: "Post received successfully"
+    })
+}
+
+export const updateNote = (req, res) => {
+    res.status(201).json({
+        message: "Post received successfully"
+    })
+}
+
+export const deleteNote = (req, res) => {
+    res.status(201).json({
+        message: "Post received successfully"
+    })
+}
